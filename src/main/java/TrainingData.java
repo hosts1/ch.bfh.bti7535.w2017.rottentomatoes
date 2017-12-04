749070f9ebdaca9a2d7e98c73a5b2edb0b558579
Kevin Haeni <khaeni@EMEA.globalcsc.net>
2017-12-04 12:17:29 +0100

    gitignore

--- src/main/java/TrainingData.java
+++ src/main/java/TrainingData.java
@@ -2,17 +2,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TrainingData {
+    public List<String> pos = Collections.emptyList();
+    public List<String> neg = Collections.emptyList();
 
     public TrainingData(String path) {
+        this.pos = getRawData(path + "//pos");
+        this.neg = getRawData(path + "//neg");
+    }
+
+    public List<String> getRawData(String path) {
         try (Stream<Path> paths = Files.walk(Paths.get(path))) {
-            paths
+            return paths
                     .filter(Files::isRegularFile)
-                    .forEach(System.out::println);
+                    .map(this::readTrainingFile)
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return Collections.emptyList();
+    }
+
+    private String readTrainingFile(Path path)
+    {
+        String fileName = path.getFileName().toString();
+        System.out.println(fileName);
+        // Todo: read training file
+        return new String(fileName);
     }
 }
